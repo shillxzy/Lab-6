f2bed53e19b3deed8e53565e1b809df4338d8a54
shillxzy <bzae03014@example.com>
2024-09-08 16:12:49 +0300

    02

---
+++ 02/Class1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Humanity
+{
+    class Person
+    {
+        private string name;
+        private string lastname;
+
+        public Person(string name, string lastname)
+        {
+            this.name = name;
+            this.lastname = lastname;
+        }
+
+    }
+
+
+    class Student : Person
+    {
+        private int numFac;
+
+        public Student(string name, string lastname, int numFac)
+            : base (name, lastname)
+        {
+            
+        }
+
+        
+    }
+
+    class Employee : Person
+    {
+        private float salary;
+        private int WorkCount;
+
+        public Employee(string name, string lastname, float salary, int WorkCount) 
+            : base(name, lastname)
+        {
+            this.salary = salary;
+            this.WorkCount = WorkCount;
+        }
+
+        public float Salaryperhour()
+        {
+            float total = 0.0f;
+            float CountOfWeek = WorkCount * 5;
+            total = (salary /  CountOfWeek);
+            return total;
+        }
+
+    }
+}
---
+++ 02/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Humanity
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Student: ");
+            string[] input = Console.ReadLine().Split(' ');
+            string name = input[0];
+            string lastname = input[1];
+            int Facnumber = int.Parse(input[2]);
+            Student student = new Student(name, lastname, Facnumber);
+
+            Console.Write("Employee: ");
+            
+
+            string[] input1 = Console.ReadLine().Split(' ');
+            string name1 = input1[0];
+            string lastname1 = input1[1];
+            float salary = float.Parse(input1[2]);
+            int hours = int.Parse(input1[3]);
+            Employee employee = new Employee(name1, lastname1, salary, hours);
+            float total = employee.Salaryperhour();
+            line();
+
+
+            Console.Write("First name: ");
+            Console.WriteLine(name);
+            Console.Write("Last name: ");
+            Console.WriteLine(lastname);
+            Console.Write("Faculty number: ");
+            Console.WriteLine(Facnumber);
+
+            Console.WriteLine();
+
+            Console.Write("First name: ");
+            Console.WriteLine(name1);
+            Console.Write("Last name: ");
+            Console.WriteLine(lastname1);
+            Console.Write("Week Salary: ");
+            Console.WriteLine(salary);
+            Console.Write("Hours per day: ");
+            Console.WriteLine(hours);
+            Console.Write("Salary per hour: ");
+            Console.WriteLine(total);
+
+        }
+
+
+        static void line()
+        {
+            Console.WriteLine("---------------------------");
+        }
+    }
+}
