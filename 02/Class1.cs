08b63f9e1ba3769d7b4f5f26765f2382148bf865
shillxzy <bzae03014@example.com>
2024-09-08 20:30:12 +0300

    lab

--- 02/Class1.cs
+++ 02/Class1.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Humanity
 {
@@ -14,8 +15,51 @@ namespace Humanity
 
         public Person(string name, string lastname)
         {
-            this.name = name;
-            this.lastname = lastname;
+            Name = name;
+            Lastname = lastname;
+        }
+
+        public string Name
+        {
+            get { return name; }
+
+            set
+            {
+                if (!char.IsUpper(value[0]))
+                {
+                    throw new("Expected upper case letter!");
+                }
+                else if (value.Length < 3)
+                {
+                    throw new("Expected length at least 4 symbols!");
+                }
+                else
+                {
+                    name = value;
+                }
+            
+            }
+        }
+
+        public string Lastname
+        {
+            get { return lastname; }
+
+            set
+            {
+                if (!char.IsUpper(value[0]))
+                {
+                    throw new("Expected upper case letter!");
+                }
+                else if (value.Length < 2)
+                {
+                    throw new("Expected length at least 3 symbols!");
+                }
+                else
+                {
+                    lastname = value;
+                }
+            }
         }
 
     }
@@ -28,7 +72,33 @@ namespace Humanity
         public Student(string name, string lastname, int numFac)
             : base (name, lastname)
         {
-            
+            FacultyNumber = numFac;
+        }
+
+        public void Display()
+        {
+            Console.Write("First name: ");
+            Console.WriteLine(Name);
+            Console.Write("Last name: ");
+            Console.WriteLine(Lastname);
+            Console.Write("Faculty number: ");
+            Console.WriteLine(FacultyNumber);
+        }
+
+        public int FacultyNumber
+        {
+            get { return numFac; }
+            set
+            {
+                if (value < 5 && value > 10)
+                {
+                    throw new("Invalid faculty number! Should be in range [5..10] symbols.");
+                }
+                else
+                {
+                    numFac = value;
+                }
+            }
         }
 
         
@@ -42,8 +112,8 @@ namespace Humanity
         public Employee(string name, string lastname, float salary, int WorkCount) 
             : base(name, lastname)
         {
-            this.salary = salary;
-            this.WorkCount = WorkCount;
+            WeekSalary = salary;
+            WorkOfCount = WorkCount;
         }
 
         public float Salaryperhour()
@@ -53,6 +123,55 @@ namespace Humanity
             total = (salary /  CountOfWeek);
             return total;
         }
+
+        public void Display()
+        {
+            Console.Write("First name: ");
+            Console.WriteLine(Name);
+            Console.Write("Last name: ");
+            Console.WriteLine(Lastname);
+            Console.Write("Week Salary: ");
+            Console.WriteLine(WeekSalary);
+            Console.Write("Hours per day: ");
+            Console.WriteLine(WorkOfCount);
+        }
+
+        public float WeekSalary
+        {
+            get { return salary; }
+            set 
+            { 
+                if (value < 10)
+                {
+                    throw new($"Expected value mismatch!Argument: {salary}");
+                }
+                else
+                    
+                {
+                    salary = value;
+                }
+            }
+        }
+
+        public float WorkOfCount
+        {
+            get { return WorkCount; }
+
+            set
+            {
+                if (value < 1 || value > 12)
+                {
+                    throw new ("Expected value mismatch! Argument: workHoursPerDay");
+                }
+                else
+                {
+                    WorkCount = (int)value;
+                }
+            }
+        }
+
+
+
 
     }
 }
--- 02/Program.cs
+++ 02/Program.cs
@@ -4,52 +4,45 @@
     {
         static void Main()
         {
-            Console.Write("Student: ");
-            string[] input = Console.ReadLine().Split(' ');
-            string name = input[0];
-            string lastname = input[1];
-            int Facnumber = int.Parse(input[2]);
-            Student student = new Student(name, lastname, Facnumber);
+            try
+            {
+                Console.Write("Student: ");
+                string[] input = Console.ReadLine().Split(' ');
+                string name = input[0];
+                string lastname = input[1];
+                int Facnumber = int.Parse(input[2]);
+                Student student = new Student(name, lastname, Facnumber);
 
-            Console.Write("Employee: ");
-            
-
-            string[] input1 = Console.ReadLine().Split(' ');
-            string name1 = input1[0];
-            string lastname1 = input1[1];
-            float salary = float.Parse(input1[2]);
-            int hours = int.Parse(input1[3]);
-            Employee employee = new Employee(name1, lastname1, salary, hours);
-            float total = employee.Salaryperhour();
-            line();
+                Console.Write("Employee: ");
 
 
-            Console.Write("First name: ");
-            Console.WriteLine(name);
-            Console.Write("Last name: ");
-            Console.WriteLine(lastname);
-            Console.Write("Faculty number: ");
-            Console.WriteLine(Facnumber);
+                string[] input1 = Console.ReadLine().Split(' ');
+                string name1 = input1[0];
+                string lastname1 = input1[1];
+                float salary = float.Parse(input1[2]);
+                int hours = int.Parse(input1[3]);
+                Employee employee = new Employee(name1, lastname1, salary, hours);
+                float total = employee.Salaryperhour();
+                Console.WriteLine("---------------------------");
 
-            Console.WriteLine();
+                student.Display();
 
-            Console.Write("First name: ");
-            Console.WriteLine(name1);
-            Console.Write("Last name: ");
-            Console.WriteLine(lastname1);
-            Console.Write("Week Salary: ");
-            Console.WriteLine(salary);
-            Console.Write("Hours per day: ");
-            Console.WriteLine(hours);
-            Console.Write("Salary per hour: ");
-            Console.WriteLine(total);
+                Console.WriteLine();
+
+                employee.Display();
+
+                Console.Write("Salary per hour: $");
+                total = (float)Math.Round(total, 2);
+                Console.WriteLine(total);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
 
         }
 
 
-        static void line()
-        {
-            Console.WriteLine("---------------------------");
-        }
+        
     }
 }
