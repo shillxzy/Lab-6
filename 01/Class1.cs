fde83500bcb4d95882ba5a823aa142ecd55dddd5
shillxzy <bzae03014@example.com>
2024-09-08 15:28:30 +0300

    first commit

---
+++ 01/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore
+{
+    internal class Book
+    {
+        private string author;
+        private string title;
+        private decimal price;
+
+        public Book(string author, string title, decimal price)
+        {
+            this.author = author;
+            this.title = title;
+            this.price = price;
+        }
+
+
+        public void Display()
+        {
+            Console.Write("Author: ");
+            Console.WriteLine(author);
+            Console.Write("Title: ");
+            Console.WriteLine(title);
+            Console.Write("Price: $");
+            Console.WriteLine(Price); 
+        }
+
+        public virtual decimal Price
+        {
+            get { return price; }
+        }
+
+    }
+
+    internal class GoldenEditionBook : Book
+    {
+        public GoldenEditionBook(string author, string title, decimal price) 
+            : base (author, title, price)
+        {
+        }
+
+        public override decimal Price => base.Price * 1.3m;
+         
+    }
+}
---
+++ 01/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Bookstore
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Author: ");
+            string author = Console.ReadLine();
+            Console.Write("Title: ");
+            string title = Console.ReadLine();
+            Console.Write("Price: ");
+            decimal price = decimal.Parse(Console.ReadLine());
+
+            Book book = new Book(author, title, price);
+            GoldenEditionBook goldenEditionBook = new GoldenEditionBook(author, title, price);
+
+            line();
+
+            Console.WriteLine("Type: Book");
+            book.Display();
+
+            line();
+
+            Console.WriteLine("Type: GoldenEditionBook");
+            goldenEditionBook.Display();
+
+        }
+
+
+        static void line()
+        {
+            Console.WriteLine("---------------------------");
+        }
+    }
+}
